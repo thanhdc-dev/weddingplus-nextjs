80e2bb929dd9c82d2eb4bc9a707b90705d6ded13
Thanhdc <dev.thanhdc@gmail.com>
2023-10-17 21:22:32 +0700

    fix: move sitemap to app folder

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import config from './libs/config';
+import config from '../libs/config';
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
